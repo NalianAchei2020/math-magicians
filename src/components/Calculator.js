f4cbd0458e4b69de6d5305ca85a1d277133900a8
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-07 13:35:25 +0100

    add styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,79 +7,114 @@ function Calculator() {
       <div className="calculator">
         <table>
           <tbody>
-            <tr>
-              <td>
+            <tr className="inputNum">
+              <td colSpan={4}>
                 <input type="text" value={0} readOnly />
               </td>
             </tr>
             <tr>
-              <td>
-                <button type="button">AC</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  AC
+                </button>
               </td>
-              <td>
-                <button type="button">+/-</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  +/-
+                </button>
               </td>
-              <td>
-                <button type="button">%</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  %
+                </button>
               </td>
-              <td>
-                <button type="button">{String.fromCharCode(247)}</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button type="button">7</button>
-              </td>
-              <td>
-                <button type="button">8</button>
-              </td>
-              <td>
-                <button type="button">9</button>
-              </td>
-              <td>
-                <button type="button">x</button>
+              <td className="Ngold">
+                <button type="button" className="Ngold">
+                  {String.fromCharCode(247)}
+                </button>
               </td>
             </tr>
             <tr>
-              <td>
-                <button type="button">4</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  7
+                </button>
               </td>
-              <td>
-                <button type="button">5</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  8
+                </button>
               </td>
-              <td>
-                <button type="button">6</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  9
+                </button>
               </td>
-              <td>
-                <button type="button">-</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button type="button">1</button>
-              </td>
-              <td>
-                <button type="button">2</button>
-              </td>
-              <td>
-                <button type="button">3</button>
-              </td>
-              <td>
-                <button type="button">+</button>
+              <td className="Ngold">
+                <button type="button" className="Ngold">
+                  x
+                </button>
               </td>
             </tr>
             <tr>
-              <td>
-                <button type="button">0</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  4
+                </button>
               </td>
-              <td>
-                <button type="button"> </button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  5
+                </button>
               </td>
-              <td>
-                <button type="button">=</button>
+              <td className="gold">
+                <button type="button" className="gold">
+                  6
+                </button>
               </td>
-              <td>
-                <button type="button">{String.fromCharCode(247)}</button>
+              <td className="Ngold">
+                <button type="button" className="Ngold">
+                  -
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="gold">
+                <button type="button" className="gold">
+                  1
+                </button>
+              </td>
+              <td className="gold">
+                <button type="button" className="gold">
+                  2
+                </button>
+              </td>
+              <td className="gold">
+                <button type="button" className="gold">
+                  3
+                </button>
+              </td>
+              <td className="Ngold">
+                <button type="button" className="Ngold">
+                  +
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} className="gold">
+                <button type="button" className="gold zero">
+                  0
+                </button>
+              </td>
+              <td className="gold">
+                <button type="button" className="gold dot">
+                  .
+                </button>
+              </td>
+              <td className="Ngold">
+                <button type="button" className="Ngold">
+                  =
+                </button>
               </td>
             </tr>
           </tbody>

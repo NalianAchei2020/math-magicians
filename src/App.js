7c30f22b48f64302162361e13ac19c51733246b5
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-07 12:47:21 +0100

    import calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Calculator from './components/Calculator';
+
 function App() {
   return (
     <div className="App">
-      <h5>SetUp React App</h5>
+      <Calculator />
     </div>
   );
 }

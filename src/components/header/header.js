7c7108314dfc679b51a38dd224eb5fd5969d0a13
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-16 10:46:42 +0100

    add styling to homePage
    add styling to homePage

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
     <div className="header">
       <h2>Math Matheticians</h2>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/calculator">Calculator</Link>
-        <Link to="/qoutes">Qoutes</Link>
+      <nav className="nav">
+        <a href="/">Home</a>
+        <a href="/calculator">Calculator</a>
+        <a href="/qoute">Qoutes</a>
       </nav>
     </div>
   );
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Container } from '@mui/material';
+import './home.css';
 
 function Home() {
   return (
-    <div>
-      <Container />
-      <p>Home</p>
+    <div className="home">
+      <Container fixed>
+        <h3>Welcome to our page</h3>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            ut nunc ac augue gravida aliquet. Sed ut suscipit justo. Phasellus
+            tincidunt tortor non congue v estibulum. Fusce congue maximus
+            dictum. Vivamus elementum tincidunt luctus. Vivamus sagittis velit
+            vitae tincidunt fringilla. Sed cursus augue et tellus tincidunt
+            consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum ut nunc ac augue gravida aliquet. Sed ut suscipit justo.
+            Phasellus tincidunt tortor non congue vestibulum. Fusce congue
+            maximus dictum. Vivamus elementum tincidunt luctus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            ut nunc ac augue gravida aliquet. Sed ut suscipit justo. Phasellus
+            tincidunt tortor non congue v estibulum. Fusce congue maximus
+            dictum. Vivamus elementum tincidunt luctus. Vivamus sagittis velit
+            vitae tincidunt fringilla. Sed cursus augue et tellus tincidunt
+            consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum ut nunc ac augue gravida aliquet. Sed ut suscipit justo.
+            Phasellus tincidunt tortor non congue vestibulum. Fusce congue
+            maximus dictum. Vivamus elementum tincidunt luctus.
+          </p>
+        </div>
+      </Container>
     </div>
   );
 }

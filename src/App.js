5d13bd7da3ac8ccfea736b00a02cc030d6f7a533
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-16 09:07:40 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Qoutes from './components/qoutes';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/qoutes" element={<Qoutes />} />
+        </Routes>
+      </Router>
       <Calculator />
       <Qoutes />
     </div>
